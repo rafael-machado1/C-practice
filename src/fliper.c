44c9c9612807d25f9c2b1b9c873a19392c356ddd
rafael-machado1 <rafaelmachadocd37@gmail.com>
2026-02-23 23:13:43 -0300

    feat: Exercício.

---
+++ src/fliper.c
@@ -0,0 +1,42 @@
+/*Flíper é um tipo de jogo onde uma bolinha de metal cai por um labirinto de caminhos até chegar na parte de baixo 
+do labirinto. A quantidade de pontos que o jogador ganha depende do caminho que a bolinha seguir. O jogador pode 
+controlar o percurso da bolinha mudando a posição de algumas portinhas do labirinto. Cada portinha pode estar na 
+posição 0, que significa virada para a esquerda, ou na posição 1 que quer dizer virada para a direita. Considere o 
+líper da figura abaixo, que tem duas portinhas. A portinha P está na posição 1 e a portinha R, na posição 0. Desse 
+jeito, a bolinha vai cair pelo caminho B.
+
+Você deve escrever um programa que, dadas as posições das portinhas P e R, neste flíper da figura, diga por qual 
+dos três caminhos, A, B ou C, a bolinha vai cair!
+
+Entrada
+A entrada é composta por apenas uma linha contendo dois números P (0 ou 1) e R (0 ou 1), indicando as posições das 
+duas portinhas do flíper da figura.
+*/
+
+/*
+                                  BOLA
+                                /      \
+                               /        \
+                              /       P  \
+                             /       /\   \
+                            /       /  \   \
+                           /        \   \   \
+                          /          \   \   \
+                         /       R    \   \   \
+                        /       /\     \   \   \
+                       /       /  \     \   \   \
+                      /       /    \     \   \   \ 
+*/
+
+#include <stdio.h>
+
+int main() {
+  int P,R;
+
+  scanf("%d%d",&P,&R);
+
+
+
+  return 0;
+
+}
